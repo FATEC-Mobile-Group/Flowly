fb481daf65acc31faf2bb855db937b6050491746
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2025-11-26 10:50:33 -0300

    teste routes

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
+const fs = require('fs');
+
+try {
+  console.log("Conteúdo da pasta /routes na produção:");
+  console.log(fs.readdirSync("./routes"));
+} catch (e) {
+  console.log("Não consegui ler a pasta /routes");
+}
+
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB conectado');
