cc1c2aee1df6861595eae7bd1f81d2473a4c3a1d
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2025-11-27 18:14:25 -0300

    teste de rota botao verificar

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
   })
   .catch((err) => console.error(err));
-  
+
 // Middleware
 app.use(cors({
   origin: '*',
