c218a7fed416384f823b32c1a29d4e9d4219ea7f
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2025-11-27 22:30:26 -0300

    alteraçoes

--- frontend/FlowlyFront/MauiProgram.cs
+++ frontend/FlowlyFront/MauiProgram.cs
@@ -11,8 +11,9 @@ namespace FlowlyFront
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Poppins-Regular.ttf", "Poppins");
+                    fonts.AddFont("Poppins-SemiBold.ttf", "PoppinsSemiBold");
+                    fonts.AddFont("Poppins-Bold.ttf", "PoppinsBold");
                 });
 
 #if DEBUG
